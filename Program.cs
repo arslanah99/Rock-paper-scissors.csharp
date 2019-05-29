9b69ca624944dff3b5cf8e940423cdce8d6dd758
arslanah99 <Arah09@hotmail.com>
2019-05-29 17:50:11 -0600

    added if statements

--- Program.cs
+++ Program.cs
@@ -6,9 +6,12 @@
         {
             string inputPlayer, inputCPU;
             int randomInt;
+            int scorePlayer = 0;
+            int scoreCPU = 0;
 
             Console.WriteLine("Choose between, ROCK, PAPER and SCISSORS:    ");
             inputPlayer = Console.ReadLine();
+            inputPlayer = inputPlayer.ToUpper();
 
             Random rnd = new Random();
 
@@ -18,10 +21,28 @@
                 case 1:
                     inputCPU = "ROCK";
                     Console.WriteLine("Computer chose ROCK!");
+                    if(inputPlayer == "ROCK"){
+                        Console.WriteLine("DRAW!!!!!\n\n");
+                    } else if (inputPlayer == "PAPER"){
+                        Console.WriteLine("PLAYER WINS!!!!\n\n");
+                        scorePlayer++;
+                    } else if(inputPlayer == "SCISSORS"){
+                        Console.WriteLine("CPU WINS!!!!!\n\n");
+                        scoreCPU++;
+                    }
                     break;
                 case 2:
                     inputCPU = "PAPER";
                     Console.WriteLine("Computer chose PAPER!");
+                    if(inputPlayer == "PAPER"){
+                        Console.WriteLine("DRAW!!!!!\n\n");
+                    } else if (inputPlayer == "SCISSORS"){
+                        Console.WriteLine("PLAYER WINS!!!!\n\n");
+                        scorePlayer++;
+                    } else if(inputPlayer == "ROCK"){
+                        Console.WriteLine("CPU WINS!!!!!\n\n");
+                        scoreCPU++;
+                    }
                     break;
                 case 3:
                     inputCPU = "SCISSORS";

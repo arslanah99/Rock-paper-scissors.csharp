f12ababb44274a872fd23433559ea13e1f3b833b
arslanah99 <Arah09@hotmail.com>
2019-05-29 17:41:58 -0600

    added computer to say either rock, paper or scissors

--- Program.cs
+++ Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
-
-namespace c_RPS
+﻿namespace ROCKPAPERSCISSORS
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputPlayer, inputCPU;
+            int randomInt;
+
+            Console.WriteLine("Choose between, ROCK, PAPER and SCISSORS:    ");
+            inputPlayer = Console.ReadLine();
+
+            Random rnd = new Random();
+
+            randomInt = rnd.Next(1, 4);
+
+            switch (randomInt){
+                case 1:
+                    inputCPU = "ROCK";
+                    Console.WriteLine("Computer chose ROCK!");
+                    break;
+                case 2:
+                    inputCPU = "PAPER";
+                    Console.WriteLine("Computer chose PAPER!");
+                    break;
+                case 3:
+                    inputCPU = "SCISSORS";
+                    Console.WriteLine("Computer chose SCISSORS!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid entry");
+                    break;
+            }
         }
     }
 }
